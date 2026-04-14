e30939b269b62f0332702c4dfda539231aac4dea
yanghuaxuan <yanghuaxuan@gmail.com>
2026-04-14 01:34:43 -0700

    Need to set clk frequency as parameter

--- cocotb/test_wb_sdr_mt48lc16m16a2_7e.py
+++ cocotb/test_wb_sdr_mt48lc16m16a2_7e.py
@@ -11,25 +11,25 @@ import random
 async def test_reset(dut):
     clk_i = dut.clk_i
     rst_i = dut.rst_i
+    sys_clk_mhz = int(dut.sys_clk_mhz_p.value)
 
-    Clock(clk_i, 100, unit="us").start()
+    Clock(clk_i, sys_clk_mhz, unit="us").start()
     await reset_sequence(clk_i, rst_i)
 
 @cocotb.test()
 async def test_initialization(dut):
     clk_i = dut.clk_i
     rst_i = dut.rst_i
+    sys_clk_mhz = int(dut.sys_clk_mhz_p.value)
 
-    clk_freq_mhz = 100
-
-    Clock(clk_i, 1/clk_freq_mhz, unit="us").start()
+    Clock(clk_i, 1/sys_clk_mhz, unit="us").start()
     await reset_sequence(clk_i, rst_i)
 
     """
     since vanilla Wishbone does not really have a ready_o interface without
     sending a read/write request, waiting 200us will do
     """
-    await ClockCycles(clk_i, clk_freq_mhz * 200)
+    await ClockCycles(clk_i, sys_clk_mhz * 200)
 
 tests =[
     'test_reset',
@@ -38,7 +38,7 @@ tests =[
 
 proj_path = Path("./src").resolve()
 sources = [ proj_path / "dram" / "wb_sdr_mt48lc16m16a2_7e.sv"]
-parameters = {}
+parameters = { "sys_clk_mhz_p": 100 }
 module_name = "test_wb_sdr_mt48lc16m16a2_7e"
 hdl_toplevel="wb_sdr_mt48lc16m16a_7e"
 
