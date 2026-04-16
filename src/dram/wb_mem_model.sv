b7b74047c16170eb48df90f590ea67806594cda8
Adi-0 <20aditya02@gmail.com>
2026-04-16 05:36:57 -0700

    parameterize wb_mem_model.sv with DATA_W so we can go between 32, 64, etc

--- src/dram/wb_mem_model.sv
+++ src/dram/wb_mem_model.sv
@@ -2,29 +2,31 @@
 // Behavioral Wishbone B4 pipelined ram (technically SRAM slave but you get the idea..)
 
 module wb_mem_model #(
-    parameter DEPTH_LOG2 = 10 // 1K words is 4 KB
+    parameter DEPTH_LOG2 = 10, // 1K words is 4 KB
+    parameter DATA_W = 32
 )(
     input  logic        clk_i,
     input  logic        rst_i,
 
     // actual WB4 slave
-    input  logic [31:0] adr_i,
-    input  logic [31:0] dat_i,
-    output logic [31:0] dat_o,
+    input  logic [DATA_W-1:0] adr_i,
+    input  logic [DATA_W-1:0] dat_i,
+    output logic [DATA_W-1:0] dat_o,
     input  logic        we_i,
     input  logic        stb_i,
     input  logic        cyc_i,
-    input  logic [3:0]  sel_i,
+    input  logic [DATA_W/8-1:0]  sel_i,
     output logic        ack_o
 );
 
     localparam DEPTH = 1 << DEPTH_LOG2;
+    localparam BYTE_BITS = $clog2(DATA_W/8);
 
-    logic [31:0] mem [DEPTH];
+    logic [DATA_W-1:0] mem [DEPTH];
     logic [DEPTH_LOG2-1:0] addr_w;
     logic valid_cycle_w;
 
-    assign addr_w        = adr_i[DEPTH_LOG2+1:2];  // word aligned
+    assign addr_w        = adr_i[DEPTH_LOG2+BYTE_BITS-1:BYTE_BITS];  // word aligned
     assign valid_cycle_w = cyc_i & stb_i;
 
     // single-cyc ack
@@ -41,10 +43,9 @@ module wb_mem_model #(
     //write
     always_ff @(posedge clk_i) begin 
         if (valid_cycle_w & we_i & ~ack_o) begin
-            if (sel_i[0]) mem[addr_w][ 7: 0] <= dat_i[ 7: 0];
-            if (sel_i[1]) mem[addr_w][15: 8] <= dat_i[15: 8];
-            if (sel_i[2]) mem[addr_w][23:16] <= dat_i[23:16];
-            if (sel_i[3]) mem[addr_w][31:24] <= dat_i[31:24];
+            for (int b = 0; b < DATA_W/8; b++) begin
+                if (sel_i[b]) mem[addr_w][b*8 +: 8] <= dat_i[b*8 +: 8];
+            end
         end
     end
 
