bbd332e8c6b1ef102d917d399cf97a99b929362d
yanghuauxan <yanghuauxan@gmail.com>
2026-04-20 11:43:01 -0700

    fix issue with some simulators failing to dump outputs due to not properly catching exceptions

--- cocotb/runner.py
+++ cocotb/runner.py
@@ -48,5 +48,7 @@ def run_test(parameters, sources, module_name, hdl_toplevel, testcase=None, sims
 
         try:
             runner.test(testcase=testcase, test_args=test_args, hdl_toplevel=hdl_toplevel, test_module=module_name, waves=True)
-        except:
-            print(f"Test '{case_name}' with {sim} failed")
+        except BaseException as e:
+            import traceback
+            print(f"Test '{case_name}' with {sim} failed: {e}")
+            traceback.print_exc()
