d1685b889e115f7e6d52b5ab9b4ed66fe7b80469
yanghuauxan <yanghuauxan@gmail.com>
2026-04-21 12:45:19 -0700

    Print exception too + ordering

--- cocotb/runner.py
+++ cocotb/runner.py
@@ -50,5 +50,6 @@ def run_test(parameters, sources, module_name, hdl_toplevel, testcase=None, sims
             runner.test(testcase=testcase, test_args=test_args, hdl_toplevel=hdl_toplevel, test_module=module_name, waves=True)
         except BaseException as e:
             import traceback
-            print(f"Test '{case_name}' with {sim} failed: {e}")
+            print(e)
             traceback.print_exc()
+            print(f"Test '{case_name}' with {sim} failed: {e}")
