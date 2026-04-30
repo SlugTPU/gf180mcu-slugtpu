e083ed108f416de9f7c20953761f163dda4ea514
Shreyas Gupta <shreyas_gupta@outlook.com>
2026-04-30 00:47:50 -0700

    control top wip

---
+++ cocotb/test_control_top.py
@@ -0,0 +1,99 @@
+import pytest
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import FallingEdge, Timer, RisingEdge, ReadOnly
+from pathlib import Path
+from shared import reset_sequence, clock_start
+from runner import run_test
+import random
+
+
+# ---------------------------------------------------------------------------
+# Reset helper
+# ---------------------------------------------------------------------------
+
+async def do_reset(dut):
+    # Instruction interface
+    dut.instruction_data_i.value  = 0
+    dut.instruction_valid_i.value = 0
+
+    # DMA/DRAM control inputs
+    dut.dma_busy.value = 0
+    dut.dma_done.value = 0
+
+    # DRAM->SRAM stream
+    dut.dram2sram_valid_i.value = 0
+    dut.dram2sram_data_i.value  = 0
+
+    # SRAM->DRAM stream
+    dut.sram2dram_ready_i.value = 0
+
+    # TPU state
+    dut.tpu_state_i.value = 0
+
+    await clock_start(dut.clk_i)
+    await reset_sequence(dut.clk_i, dut.rst_i)
+    await FallingEdge(dut.clk_i)
+
+@cocotb.test()
+async def test_reset(dut):
+    """Verify the core comes out of reset without assertions firing."""
+    await do_reset(dut)
+    await FallingEdge(dut.clk_i)
+    assert dut.instruction_ready_o.value == 1
+
+tests = [
+    'test_reset',
+]
+
+proj_path = Path("./src").resolve()
+sources = [
+    proj_path / "control/control_sram.sv",
+    proj_path / "sram/sram_1x256.sv",
+    proj_path / "control/control_buffer.sv",
+    proj_path / "control/control_decoder.sv",
+    proj_path / "compute_core.sv",
+    proj_path / "scalar_units/scalar_stage_sram.sv",
+    proj_path / "scalar_units/scalar_stage.sv",
+    proj_path / "scalar_units/scalar_pipe.sv",
+    proj_path / "scalar_units/add_n.sv",
+    proj_path / "scalar_units/scale_n.sv",
+    proj_path / "scalar_units/relu_n.sv",
+    proj_path / "scalar_units/load_data.sv",
+    proj_path / "scalar_units/quantizer_mul.sv",
+    proj_path / "common/elastic.sv",
+    proj_path / "common/shift.sv",
+    proj_path / "sram/memory_transaction.sv",
+    proj_path / "sram/sram_8x256_full.sv",
+    proj_path / "sram/sram_8x256.sv",
+    proj_path / "sysray/sysray_nxn.sv",
+    proj_path / "sysray/pe.sv",
+    proj_path / "sysray/mxu.sv",
+    proj_path / "common/counter.sv",
+    proj_path / "tri_shift.sv",
+    "./ip/gf180mcu_ocd_ip_sram.git/cells/gf180mcu_ocd_ip_sram__sram256x8m8wm1/gf180mcu_ocd_ip_sram__sram256x8m8wm1.v",
+]
+
+
+@pytest.mark.parametrize("testcase", tests)
+def test_compute_core_each(testcase):
+    """Runs each test independently. Continues on test failure."""
+    run_test(
+        parameters={},
+        sources=sources,
+        module_name="test_control_top",
+        hdl_toplevel="control_top",
+        testcase=testcase,
+        sims=['icarus'],
+    )
+
+
+def test_compute_core_all():
+    """Runs all tests sequentially in one simulation."""
+    run_test(
+        parameters={},
+        sources=sources,
+        module_name="test_control_top",
+        hdl_toplevel="control_top",
+        sims=['icarus'],
+    )
--- src/control/control_top.sv
+++ src/control/control_top.sv
@@ -33,25 +33,49 @@ module control_top #(
     output pc_ready_o,
 
     // TPU STATE
-    output [2:0] tpu_state_o,
+    output [1:0] tpu_state_o,
     output INTERNAL_ERROR_O
 );
-
+    logic buffer_ready_in, buffer_ready_out, buffer_valid_in, buffer_valid_out;
+    logic decoder_ready_in, decoder_ready_out, decoder_valid_in, decoder_valid_out;
+    logic instruction_valid, instruction_ready;
+    always_comb begin : dram_access_mux
+        
+    end
     /*
     TPU STATE
     */
-    typedef enum bit[2:0] 
+    typedef enum bit[1:0] 
         {RST        = 2'b00    // rst_i is high
         ,IDLE       = 2'b01    // after reset or after exit
         ,INIT_PC    = 2'b10    // Initalizing Program counter
         ,COMPUTE    = 2'b11    // In compute mode
         }
     tpu_state_t;
+    tpu_state_t tpu_state_d;
+    assign pc_ready_o = tpu_state_o == IDLE;
 
-    always_ff @( posedge clk_i ) begin : tpu_state_block
-        if(rst_i)
+    // This should be a case based FSM
+    always_comb begin : tpu_state_logic
+        tpu_state_d = tpu_state_q;
+        if(tpu_state_q == RST & ~rst_i) begin
+            tpu_state_d = IDLE;
+        end
+        if(pc_valid_i & tpu_state_q == IDLE) begin
+            tpu_state_d = INIT_PC;
+        end
+        if(tpu_state_q == INIT_PC & ~buffer_ready_out)begin
+            tpu_state_d = COMPUTE;
+        end
+    end
+
+    always_ff @( posedge clk_i ) begin : tpu_state_dff
+        if(rst_i) begin
             tpu_state_o <= RST;
-        // TODO: else
+        end
+        else begin
+            tpu_state_o <= tpu_state_d;
+        end
     end
     
     control_sram #(
@@ -61,51 +85,50 @@ module control_top #(
         .rst_i(rst_i),
 
         .wr_data_i(),
-        .valid_i(),
-        .ready_o(),
+        .valid_i(buffer_valid_out),
+        .ready_o(buffer_ready_in),
 
-        .ready_i(),
+        .ready_i(instruction_ready),
         .rd_data_o(),
-        .valid_o(),
+        .valid_o(instruction_valid),
 
         .is_full_o()
     );
 
     control_buffer #(
-        .DRAM_WIDTH       (),
-        .CONTROL_WIDTH    (),
-        .DRAM_ADDR_WIDTH  (),
-        .DEPTH_LOG2_P     ()
+        .DRAM_WIDTH       (DRAM_DATA_WIDTH),
+        .CONTROL_WIDTH    (CONTROL_WIDTH),
+        .DRAM_ADDR_WIDTH  (DRAM_ADDR_WIDTH)
     ) control_buffer_inst (
-        .clk_i            (),
-        .rst_i            (),
+        .clk_i            (clk_i),
+        .rst_i            (rst_i),
 
-        .pc_in            (),
-        .pc_valid_i       (),
+        .pc_in            (pc_in),
+        .pc_valid_i       (pc_valid_i),
 
-        .wr_data_i        (),
-        .wr_valid_i       (),
-        .wr_ready_o       (),
+        .wr_data_i        (dram2sram_data_i),
+        .wr_valid_i       (buffer_valid_in),
+        .wr_ready_o       (buffer_ready_out),
 
         .pc_out           (),
 
         .rd_data_o        (),
-        .rd_valid_o       (),
-        .rd_ready_i       ()
+        .rd_valid_o       (buffer_valid_out),
+        .rd_ready_i       (buffer_ready_in)
     );
 
     control_decoder #(
-        .SRAM_ADDR_WIDTH   (),
-        .DRAM_ADDR_WIDTH   (),
-        .DRAM_COUNTER_WIDTH(),
-        .DRAM_DATA_WIDTH   ()
+        .SRAM_ADDR_WIDTH   (SRAM_ADDR_WIDTH),
+        .DRAM_ADDR_WIDTH   (DRAM_ADDR_WIDTH),
+        .DRAM_COUNTER_WIDTH(DRAM_COUNTER_WIDTH),
+        .DRAM_DATA_WIDTH   (DRAM_DATA_WIDTH)
     ) control_decoder_inst (
-        .clk_i                (),
-        .rst_i                (),
+        .clk_i                (clk_i),
+        .rst_i                (rst_i),
 
-        .instruction_ready_o  (),
+        .instruction_ready_o  (instruction_ready),
         .instruction_data_i   (),
-        .instruction_valid_i  (),
+        .instruction_valid_i  (instruction_ready),
 
         .dram_start_addr_o    (),
         .dram_word_count_o    (),
@@ -114,12 +137,12 @@ module control_top #(
         .busy_o               (),
         .done_o               (),
 
-        .dram2sram_valid_i    (),
-        .dram2sram_data_i     (),
-        .dram2sram_ready_o    (),
+        .dram2sram_valid_i    (decoder_valid_in),
+        .dram2sram_data_i     (dram2sram_data_i),
+        .dram2sram_ready_o    (decoder_ready_out),
 
-        .sram2dram_valid_o    (),
-        .sram2dram_data_o     (),
-        .sram2dram_ready_i    ()
+        .sram2dram_valid_o    (decoder_valid_out),
+        .sram2dram_data_o     (sram2dram_data_o),
+        .sram2dram_ready_i    (decoder_ready_in)
     );
 endmodule
