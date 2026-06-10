2e53ab1d707a90c173cb5a586c7af48af27bbf93
yanghuaxuan <yanghuaxuan@gmail.com>
2026-06-10 15:36:27 -0700

    Add pytest as Nix dependency

--- flake.nix
+++ flake.nix
@@ -69,6 +69,7 @@
               ps: with ps; [
                 # Verification
                 cocotb
+                pytest
 
                 # For KLayout Python DRC runner
                 docopt
